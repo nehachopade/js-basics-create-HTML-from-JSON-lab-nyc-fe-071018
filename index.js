a03c6d2fbbc5921886b0fb40cd03993936eb584d
Neha Chopade <nehachopade22@gmail.com>
2018-08-23 23:25:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ document.addEventListener("DOMContentLoaded", (e) => {
   console.log(movies)
   // invoking a function here will make sure it happens on page load
   // your code here
+  const titanicButton =document.getElementById('Titanic')
+  const terminatorButton =document.getElementById('Terminator 2')
 });
