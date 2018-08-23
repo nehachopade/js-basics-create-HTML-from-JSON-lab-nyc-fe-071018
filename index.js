11eddcca1da7832f89d873caacb397fe70a4df64
Neha Chopade <nehachopade22@gmail.com>
2018-08-23 23:28:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
   const titanicButton =document.getElementById('Titanic')
   const terminatorButton =document.getElementById('Terminator 2')
+  titanicButton.addEventListener('click',(event) =>{
+    //do something here
+  })
+  terminatorButton.addEventListener('click',(event) =>{
+    //do something here
+  })
 });
+
+let title = getElementById ('title')
