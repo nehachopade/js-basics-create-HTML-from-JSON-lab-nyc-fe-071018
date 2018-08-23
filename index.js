4c0dcfa0027cf0ec04bba3d363a85bca15dfa318
Neha Chopade <nehachopade22@gmail.com>
2018-08-23 23:38:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,7 @@ function updateHTMLContent()
   let filmRating = document.getElementById ('filmRating')
   
   title.innerHTML = movies['Titanic'].title
-  
+  director.innerHTML = movies['Titanic'].director
+  genre.innerHTML = movies['Titanic'].genre
+  filmRating.innerHTML = movies['Titanic'].filmRating
 }
