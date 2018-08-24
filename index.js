8cad236bba97e3d2846027e73fcb95c2a8a333d8
Neha Chopade <nehachopade22@gmail.com>
2018-08-24 00:19:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
   document.addEventListener("DOMContentLoaded", (e) => {
+    updateHTMLContent('Titanic') 
   console.log(movies)
   const titanicButton =document.getElementById('Titanic')
   const terminatorButton =document.getElementById('Terminator 2')
